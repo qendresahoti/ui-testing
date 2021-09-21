69df67648574dec911946930ce97934e22194ed8
Qendresa Hoti <qendresa@butlerhospitality.com>
2021-09-21 10:20:43 +0200

    changes

--- snapshot-tests/react-snapshot-tests/src/components/Items.test.js
+++ snapshot-tests/react-snapshot-tests/src/components/Items.test.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import renderer from 'react-test-renderer';
 
@@ -5,5 +6,17 @@ import Items from './Items';
 
 it('renders correctly when there are no items', () => {
   const tree = renderer.create(<Items />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly when there is a single item', () => {
+  const items = ['one'];
+  const tree = renderer.create(<Items items={items} />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly when there are multiple items', () => {
+  const items = ['one', 'two', 'three'];
+  const tree = renderer.create(<Items items={items} />).toJSON();
   expect(tree).toMatchSnapshot();
 });
